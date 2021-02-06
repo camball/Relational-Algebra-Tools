d2a9c761fff60689154f51cf84518816b560a548
camball <cameronbradleyball@gmail.com>
2021-02-05 23:05:14 -0500

    Initial Commit

---
+++ Relational Algebra Tools/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Relational Algebra Tools
+//
+//  Created by Cameron Ball on 2/5/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
